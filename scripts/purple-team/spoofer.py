9fc52e0fc594895c5e48781dc63068ef6ded81f0
Henry Graves <hgraves@countyofcolusaca.gov>
2026-02-25 15:20:26 -0800

    add email spoofer script, add some misc scripts, some dont work

---
+++ scripts/purple-team/spoofer.py
@@ -0,0 +1,25 @@
+
+
+import smtplib
+from email.mime.text import MIMEText
+
+msg = MIMEText("This is a test email sent from the spoofer script.")
+
+subject_input = input("Subject: ")
+msg['Subject'] = subject_input
+
+from_input = input("From addr: ")
+msg['From'] = from_input
+
+to_input = input("To addr: ")
+msg['To'] = to_input
+
+port_input = input("Port: ")
+email_server_input = input("Email server: ")
+smpt = smtplib.SMTP(email_server_input, int(port_input))
+
+input("Press enter to send email...")
+send_ = smpt.send_message(msg)
+print("Email sent!")
+smpt.quit()
+exit()

cb51d9a349de3a15142b358046fa6341336b49a1
Henry Graves <hgraves@countyofcolusaca.gov>
2026-07-30 12:49:35 -0700

    add auto update to init

--- init.ps1
+++ init.ps1
@@ -1,4 +1,5 @@
 winget install --id Microsoft.PowerShell --source winget -h --accept-package-agreements --accept-source-agreements --disable-interactivity
+git pull
 clear
 write-host "initiating ..."
 Start-Process pwsh -Verb RunAs -ArgumentList '-File "./cutil.ps1"'

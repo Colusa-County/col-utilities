f2e25a92eb09db7b4cf5370553f932f592e74476
Henry Graves <hgraves@countyofcolusaca.gov>
2026-07-30 16:33:45 -0700

    remove acrobat scripts

--- init.ps1
+++ init.ps1
@@ -1,6 +1,6 @@
 winget install --id Microsoft.PowerShell --source winget -h --accept-package-agreements --accept-source-agreements --disable-interactivity
 git pull
-clear
+Clear-Screen
 write-host "initiating ..."
 Start-Process pwsh -Verb RunAs -ArgumentList '-File "./cutil.ps1"'
 exit

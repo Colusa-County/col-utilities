5752f4e0fb5ba1f7c6a87b65d69f8aa221df6e4b
Henry Graves <hgraves@countyofcolusaca.gov>
2026-07-30 14:40:36 -0700

    add download/install script for m365

---
+++ scripts/Install-M365Apps.ps1
@@ -0,0 +1,2 @@
+curl -L -o OfficeSetup.exe "https://c2rsetup.officeapps.live.com/c2r/download.aspx?ProductreleaseID=O365ProPlusRetail&platform=x64&language=en-us&version=O16GA"
+Start-Process pwsh -Verb RunAs -ArgumentList '-File "./OfficeSetup.exe"'
